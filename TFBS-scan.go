7025a20db92e475348a6cac85c2ace18d9a8084d
Jose Geraldo de Carvalho Pereira <jgcarvalho@usp.br>
2016-08-23 10:37:31 -0300

    simple scan - one receptor - print values to each chromossome base

--- TFBS-scan.go
+++ TFBS-scan.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"math"
 	"os"
@@ -11,6 +12,7 @@ import (
 	"github.com/biogo/biogo/seq/linear"
 )
 
+// Estrutura que armazena os dados do cromosso e os valores de energia da ligação com o receptor para cada base. O valor da ligação é anotado na primeira base do motivo
 type ChrRec struct {
 	ChrName         string
 	RecName         string
@@ -21,6 +23,7 @@ type ChrRec struct {
 	AntiSenseProb   []float32
 }
 
+// Estrutura do receptor. Nome, comprimento e tabelas de energia e probabilidade para a fita senso e antisenso
 type Rec struct {
 	Name               string
 	Length             int
@@ -30,6 +33,10 @@ type Rec struct {
 	RevCompProbTable   map[string]float32
 }
 
+// type Peaks struct {
+// }
+
+// Função de scan que atribui os valores de energia e probabilidade para cada base do cromossomo nas fitas senso e antisenso
 func (rec *Rec) Scan(chrname, chr string) ChrRec {
 	var chrRec ChrRec
 	chrRec.ChrName = chrname
@@ -40,16 +47,18 @@ func (rec *Rec) Scan(chrname, chr string) ChrRec {
 	chrRec.AntiSenseEnergy = make([]float32, len(chr))
 	chrRec.AntiSenseProb = make([]float32, len(chr))
 
-	score := make([]float32, len(chr))
-	length := 8
+	length := rec.Length
 	for i := 0; i < (len(chr) - (length - 1)); i++ {
-		score[i] = table[chr[i:i+length]]
+		chrRec.SenseEnergy[i] = rec.EnergyTable[chr[i:i+length]]
+		chrRec.SenseProb[i] = rec.ProbTable[chr[i:i+length]]
+		chrRec.AntiSenseEnergy[i] = rec.RevCompEnergyTable[chr[i:i+length]]
+		chrRec.AntiSenseProb[i] = rec.RevCompProbTable[chr[i:i+length]]
 	}
-	// fmt.Println(chr)
-	// fmt.Println(score)
-	return score
+
+	return chrRec
 }
 
+// complemento reverso
 func revcomp(s string) string {
 	rc := make([]byte, len(s))
 	for i, j := 0, len(s)-1; i < len(s); i, j = i+1, j-1 {
@@ -69,6 +78,7 @@ func revcomp(s string) string {
 	return string(rc)
 }
 
+// Le o fasta do cromossomo
 func readChr(fn string) string {
 	fastaFile, err := os.Open(fn)
 	if err != nil {
@@ -82,6 +92,7 @@ func readChr(fn string) string {
 	return seq.(*linear.Seq).String()
 }
 
+// Le a tabela de energias ordenada em ordem decrescente
 func readTable(recname, fn string) *Rec {
 	energy := make(map[string]float32)
 	prob := make(map[string]float32)
@@ -111,21 +122,51 @@ func readTable(recname, fn string) *Rec {
 		energyRevComp[motifRevComp] = score
 		probRevComp[motifRevComp] = 1.0 - (pos / float32(math.Pow(4.0, float64(len(motif)))))
 		pos += 1.0
-		// fmt.Println(motif, motifRevComp)
 	}
 
 	return &Rec{Name: recname, Length: len(motif), EnergyTable: energy, ProbTable: prob, RevCompEnergyTable: energyRevComp, RevCompProbTable: probRevComp}
 }
 
+// Temporario: imprime os valores de energia e probabilidade dos cromossomos em csv
+func print(s []float32) {
+	fmt.Println("seqname\tstart\tend\tscore")
+	l := 8
+	for i := 0; i < len(s); i++ {
+		if s[i] > 0.5 {
+			fmt.Printf("chrY\t%d\t%d\t%.2f\n", i, i+l-1, s[i])
+		}
+	}
+}
+
 func main() {
-	ch := readChr("/home/jgcarvalho/gocode/src/github.com/jgcarvalho/TFBS-scan/Hs.chrY.fasta")
-	receptor := readTable("P10589", "/home/jgcarvalho/gocode/src/github.com/jgcarvalho/TFBS-scan/P10589.csv")
-	// fmt.Println(table)
-	// scoreEnergy := scan(ch, table)
-	chrData := receptor.Scan("chrY", ch)
-	// scorePercentil := scan(ch, receptor.ProbTable)
-	fmt.Println("Done")
+	fnchr := flag.String("chr", "", "chromossome input fasta file")
+	fntable := flag.String("table", "", "receptor - energy table (IMPORTANT: descending order)")
+
+	flag.Parse()
+
+	ch := readChr(*fnchr)
+	recA := readTable("RECEPTOR NAME", *fntable)
+	chrRecA := recA.Scan("CHROMOSSOME NAME", ch)
+	print(chrRecA.SenseProb)
+
+	// ch := readChr("/home/jgcarvalho/gocode/src/github.com/jgcarvalho/TFBS-scan/Hs.chrY.fasta")
+
+	// recA := readTable("P10589", "/home/jgcarvalho/gocode/src/github.com/jgcarvalho/TFBS-scan/P10589.csv")
+	// recB := readTable("P19793", "/home/jgcarvalho/gocode/src/github.com/jgcarvalho/TFBS-scan/P19793.csv")
+	// chrRecA := recA.Scan("chrY", ch)
+	// chrRecB := recB.Scan("chrY", ch)
+	// fmt.Println("Done")
 	// fmt.Println(scoreEnergy[100000:100100])
-	fmt.Println(scorePercentil[100000:100100])
+
+	// fmt.Println(chrRecA.SenseProb[100000:100100])
+	// fmt.Println(chrRecB.SenseProb[100000:100100])
+
+	// fmt.Println(chrData.SenseEnergy[100000:100100])
+	// fmt.Println(chrData.SenseProb[100000:100100])
+	// fmt.Println(chrData.AntiSenseEnergy[100000:100100])
+	// fmt.Println(chrData.AntiSenseProb[100000:100100])
+	// fmt.Println(chrRecA.SenseProb)
+
+	// print(chrRecA.SenseProb)
 
 }
